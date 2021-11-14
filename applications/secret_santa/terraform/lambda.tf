9c89bf3e87ecdd25d89d5608674abab29ad07008
Reuben Reyes <reuben.a.reyes@gmail.com>
2021-11-13 18:54:25 -0800

    fix(secret_santa/lambda/remind): allow eventbridge schedule to invoke lambda

--- applications/secret_santa/terraform/lambda.tf
+++ applications/secret_santa/terraform/lambda.tf
@@ -19,6 +19,11 @@ locals {
 module "remind" {
   source = "terraform-aws-modules/lambda/aws"
 
+  source_path = [{
+    path     = "${local.lambda.root}/remind"
+    commands = local.lambda.deployment.source_path.commands
+  }]
+
   function_name = "${local.metadata.prefix}_remind"
   description   = "reminds people to sign up for secret santa"
   handler       = "main"
@@ -27,11 +32,21 @@ module "remind" {
 
   memory_size = 512
   timeout     = 60
+  environment_variables = {
+    DISCORD_CHANNEL_ID          = var.discord_channel_id
+    DISCORD_TOKEN_SECRET_ID     = aws_secretsmanager_secret.discord_token.id
+    DRAW_URL                    = var.draw_url
+    DYNAMODB_TABLE_PARTICIPANTS = aws_dynamodb_table.participants.id
+    REGISTER_URL                = var.register_url
+    STATIC_BUCKET_ID            = aws_s3_bucket.static.id
+  }
 
-  source_path = [{
-    path     = "${local.lambda.root}/remind"
-    commands = local.lambda.deployment.source_path.commands
-  }]
+  allowed_triggers = {
+    eventbridge = {
+      principal  = "events.amazonaws.com"
+      source_arn = aws_cloudwatch_event_rule.daily_remind.arn
+    }
+  }
 
   store_on_s3 = true
   s3_bucket   = local.lambda.deployment.s3.bucket
@@ -45,13 +60,4 @@ module "remind" {
     data.aws_iam_policy_document.read_participant_photos.json,
     data.aws_iam_policy_document.read_discord_token_secret.json,
   ]
-
-  environment_variables = {
-    DISCORD_CHANNEL_ID          = var.discord_channel_id
-    DISCORD_TOKEN_SECRET_ID     = aws_secretsmanager_secret.discord_token.id
-    DRAW_URL                    = var.draw_url
-    DYNAMODB_TABLE_PARTICIPANTS = aws_dynamodb_table.participants.id
-    REGISTER_URL                = var.register_url
-    STATIC_BUCKET_ID            = aws_s3_bucket.static.id
-  }
 }
